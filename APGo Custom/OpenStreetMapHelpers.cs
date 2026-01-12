8b80ce0bfe677eab6ea2de26a57b8b868e41eefb
thedrummonger <thedrummonger@DESKTOP>
2026-01-12 14:57:56 -0600

    Swap to Xamarin.GooglePlayServices.Location for location services on android for far more accurate tracking.

--- APGo Custom/OpenStreetMapHelpers.cs
+++ APGo Custom/OpenStreetMapHelpers.cs
@@ -4,11 +4,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#if ANDROID
+using Android.Gms.Location;
+using AndroidX.Core.Location;
+#endif
 
 namespace APGo_Custom
 {
     internal class OpenStreetMapHelpers
     {
+#if ANDROID
+        public static async void StartLocationTracking(MainPage Parent, WebView Map)
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            if (status != PermissionStatus.Granted)
+                status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+            if (status != PermissionStatus.Granted)
+                return;
+
+            Parent._isTracking = true;
+
+            var fusedLocationClient = LocationServices.GetFusedLocationProviderClient(Platform.CurrentActivity);
+
+            var locationRequest = LocationRequest.Create()
+                .SetPriority(100)
+                .SetInterval(1000)
+                .SetFastestInterval(500);
+
+            var locationCallback = new AndroidLocationCallback(Parent, Map);
+
+            await fusedLocationClient.RequestLocationUpdatesAsync(locationRequest, locationCallback);
+        }
+        private class AndroidLocationCallback : Android.Gms.Location.LocationCallback
+        {
+            private readonly MainPage _parent;
+            private readonly WebView _map;
+
+            public AndroidLocationCallback(MainPage parent, WebView map)
+            {
+                _parent = parent;
+                _map = map;
+            }
+
+            public override void OnLocationResult(Android.Gms.Location.LocationResult result)
+            {
+                if (result?.LastLocation == null || !_parent._isTracking) return;
+
+                var loc = result.LastLocation;
+                var location = new Location()
+                {
+                    Latitude = loc.Latitude,
+                    Longitude = loc.Longitude,
+                    Accuracy = loc.Accuracy,
+                    Timestamp = DateTimeOffset.FromUnixTimeMilliseconds(loc.Time)
+                };
+
+                MainThread.BeginInvokeOnMainThread(async () =>
+                {
+                    await _map.EvaluateJavaScriptAsync($"updateLocationMarker({location.Latitude}, {location.Longitude});");
+                    APLocationHelpers.CheckLocationProximity(_parent, _map, location);
+                });
+            }
+        }
+
+#else
         public static async void StartLocationTracking(MainPage Parent, WebView Map)
         {
             var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
@@ -52,6 +112,7 @@ namespace APGo_Custom
                 await Task.Delay(1000);
             }
         }
+#endif
 
         public static async Task LoadMapAsync(MainPage Parent, WebView Map)
         {
